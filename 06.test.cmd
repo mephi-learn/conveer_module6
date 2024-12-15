4c48b4a6820d984fe2e2972fa7824bce332cf25f
Vyacheslav Sitnikov <vsitnikov@gmail.com>
2024-12-15 20:32:29 +0300

    add test

---
+++ 06.test.cmd
@@ -0,0 +1,8 @@
+@echo off
+
+curl http://localhost:53080 2> NUL | findstr working
+curl http://localhost:53080 2> NUL | findstr working
+curl http://localhost:53080 2> NUL | findstr working
+curl http://localhost:53080 2> NUL | findstr working
+
+pause
---
+++ 06.test.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+curl http://localhost:53080 2>/dev/null | grep working
+curl http://localhost:53080 2>/dev/null | grep working
+curl http://localhost:53080 2>/dev/null | grep working
+curl http://localhost:53080 2>/dev/null | grep working
